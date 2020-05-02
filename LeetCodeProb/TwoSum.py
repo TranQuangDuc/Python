8f80c2b3ccfc823f2c4bbee910b05081398a4726
jhadheeraj1986 <47793209+jhadheeraj1986@users.noreply.github.com>
2020-05-02 21:45:47 +0530

    TwoSum: prob and solution

---
+++ LeetCodeProb/TwoSum.py
@@ -0,0 +1,30 @@
+'''
+Link: https://leetcode.com/problems/two-sum/
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        dic = dict()
+        for i in range(0, len(nums)):
+            dic[nums[i]] = i
+            
+        # print(dic)
+        
+        for i in range(0, len(nums)):
+            complement = target - nums[i]
+            if complement in dic:
+                # j = dic[complement]
+                return [i,dic[complement]]
